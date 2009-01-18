e24729cd2cf51db18e9cf3866911c2f117f25d8f
berendona <39708598+Barkole@users.noreply.github.com>
2009-01-18 22:18:38 +0000

    Remove not important maven helper scripts

--- mvn-dependencies.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#mvn -Dmdep.useRepositoryLayout=true \
-#    -Dmdep.copyPom=true \
-#    clean:clean \
-#    dependency:copy-dependencies
-
-mvn clean:clean \
-    dependency:copy-dependencies
