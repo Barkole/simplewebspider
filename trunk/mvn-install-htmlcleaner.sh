4e7dc7329b5c5cc0f2568627c6cd5d7c12baa7a9
berendona <berendona@0f88a7a3-3830-49a0-93f0-45b86e23c586>
2009-01-25 00:00:07 +0000

    Improvements: Don't preparse and load body, but use more streaming (152909) and Detect binary content and skip that URL (152910)

--- trunk/mvn-install-htmlcleaner.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mvn install:install-file \
-    -Dfile=lib/htmlcleaner2_1.jar \
-    -DgroupId=org.htmlcleaner \
-    -DartifactId=htmlcleaner \
-    -Dversion=2.1 \
-    -Dpackaging=jar \
-    -DgeneratePom=true
