fe8d660302418e450d11afab201385e97d7eb180
berendona <berendona@0f88a7a3-3830-49a0-93f0-45b86e23c586>
2009-08-12 19:32:05 +0000

    Update: upgrade db4o to 7.11

--- trunk/install-db4o.sh
+++ trunk/install-db4o.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-VERSION=7.10.96
-BUILD=13184
-LIB=dev/db4o-7.10/lib
+VERSION=7.11.107
+BUILD=13473
+LIB=dev/db4o-7.11/lib
 mvn install:install-file -Dfile=$LIB/bloat-1.0.jar -DgroupId=com.db4o -DartifactId=db4o-bloat -Dversion=$VERSION.$BUILD -Dpackaging=jar -DgeneratePom=true
 mvn install:install-file -Dfile=$LIB/db4o-$VERSION.$BUILD-all-java1.1.jar -DgroupId=com.db4o -DartifactId=db4o-all-java1.1 -Dversion=$VERSION.$BUILD -Dpackaging=jar -DgeneratePom=true
 mvn install:install-file -Dfile=$LIB/db4o-$VERSION.$BUILD-all-java1.2.jar -DgroupId=com.db4o -DartifactId=db4o-all-java1.2 -Dversion=$VERSION.$BUILD -Dpackaging=jar -DgeneratePom=true
@@ -30,19 +30,3 @@ mvn install:install-file -Dfile=$LIB/db4o-$VERSION.$BUILD-taj-java1.2.jar -Dgrou
 mvn install:install-file -Dfile=$LIB/db4o-$VERSION.$BUILD-taj-java5.jar -DgroupId=com.db4o -DartifactId=db4o-taj-java5 -Dversion=$VERSION.$BUILD -Dpackaging=jar -DgeneratePom=true
 mvn install:install-file -Dfile=$LIB/db4o-$VERSION.$BUILD-tools-java1.2.jar -DgroupId=com.db4o -DartifactId=db4o-tools-java1.2 -Dversion=$VERSION.$BUILD -Dpackaging=jar -DgeneratePom=true
 mvn install:install-file -Dfile=$LIB/db4o-$VERSION.$BUILD-tools-java5.jar -DgroupId=com.db4o -DartifactId=db4o-tools-java5 -Dversion=$VERSION.$BUILD -Dpackaging=jar -DgeneratePom=true
-
-#VERSION=7.4.88
-#BUILD=12908
-#LIB=dev/db4o-7.4/lib
-#mvn install:install-file -Dfile=$LIB/bloat-1.0.jar -DgroupId=com.db4o -DartifactId=db4o-bloat -Dversion=$VERSION.$BUILD -Dpackaging=jar -DgeneratePom=true
-#mvn install:install-file -Dfile=$LIB/db4o-$VERSION.$BUILD-bench.jar -DgroupId=com.db4o -DartifactId=db4o-bench -Dversion=$VERSION.$BUILD -Dpackaging=jar -DgeneratePom=true
-#mvn install:install-file -Dfile=$LIB/db4o-$VERSION.$BUILD-db4ounit.jar -DgroupId=com.db4o -DartifactId=db4o-db4ounit -Dversion=$VERSION.$BUILD -Dpackaging=jar -DgeneratePom=true
-#mvn install:install-file -Dfile=$LIB/db4o-$VERSION.$BUILD-instrumentation.jar -DgroupId=com.db4o -DartifactId=db4o-instrumentation -Dversion=$VERSION.$BUILD -Dpackaging=jar -DgeneratePom=true
-#mvn install:install-file -Dfile=$LIB/db4o-$VERSION.$BUILD-java1.1.jar -DgroupId=com.db4o -DartifactId=db4o-java1.1 -Dversion=$VERSION.$BUILD -Dpackaging=jar -DgeneratePom=true
-#mvn install:install-file -Dfile=$LIB/db4o-$VERSION.$BUILD-java1.2.jar -DgroupId=com.db4o -DartifactId=db4o-java1.2 -Dversion=$VERSION.$BUILD -Dpackaging=jar -DgeneratePom=true
-#mvn install:install-file -Dfile=$LIB/db4o-$VERSION.$BUILD-java5.jar -DgroupId=com.db4o -DartifactId=db4o-java5 -Dversion=$VERSION.$BUILD -Dpackaging=jar -DgeneratePom=true
-#mvn install:install-file -Dfile=$LIB/db4o-$VERSION.$BUILD-nqopt.jar -DgroupId=com.db4o -DartifactId=db4o-nqopt -Dversion=$VERSION.$BUILD -Dpackaging=jar -DgeneratePom=true
-#mvn install:install-file -Dfile=$LIB/db4o-$VERSION.$BUILD-osgi-test.jar -DgroupId=com.db4o -DartifactId=db4o-osgi-test -Dversion=$VERSION.$BUILD -Dpackaging=jar -DgeneratePom=true
-#mvn install:install-file -Dfile=$LIB/db4o-$VERSION.$BUILD-osgi.jar -DgroupId=com.db4o -DartifactId=db4o-osgi -Dversion=$VERSION.$BUILD -Dpackaging=jar -DgeneratePom=true
-#mvn install:install-file -Dfile=$LIB/db4o-$VERSION.$BUILD-taj.jar -DgroupId=com.db4o -DartifactId=db4o-taj -Dversion=$VERSION.$BUILD -Dpackaging=jar -DgeneratePom=true
-#mvn install:install-file -Dfile=$LIB/db4o-$VERSION.$BUILD-tools.jar -DgroupId=com.db4o -DartifactId=db4o-tools -Dversion=$VERSION.$BUILD -Dpackaging=jar -DgeneratePom=true
