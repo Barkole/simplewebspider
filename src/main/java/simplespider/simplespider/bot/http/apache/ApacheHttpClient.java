efe4adbe8a2bd4d273bba8dbd48ca7d99c00d35b
berendona <39708598+Barkole@users.noreply.github.com>
2009-12-23 23:33:39 +0000

    Update: Upgrade HTTPClient 3 to 4

--- src/main/java/simplespider/simplespider/bot/http/apache/ApacheHttpClient.java
+++ src/main/java/simplespider/simplespider/bot/http/apache/ApacheHttpClient.java
@@ -19,23 +19,28 @@ package simplespider.simplespider.bot.http.apache;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
 
-import org.apache.commons.httpclient.Header;
-import org.apache.commons.httpclient.HttpException;
-import org.apache.commons.httpclient.HttpMethod;
-import org.apache.commons.httpclient.StatusLine;
-import org.apache.commons.httpclient.URI;
-import org.apache.commons.httpclient.URIException;
-import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.protocol.ClientContext;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
 
 import simplespider.simplespider.bot.http.HttpClient;
 import simplespider.simplespider.util.ValidityHelper;
 
 public class ApacheHttpClient implements HttpClient {
-	private final org.apache.commons.httpclient.HttpClient	httpClient;
-	private HttpMethod										method	= null;
+	private final org.apache.http.client.HttpClient	httpClient;
+	private HttpGet									httpGet			= null;
+	private HttpResponse							httpResponse	= null;
 
-	ApacheHttpClient(final org.apache.commons.httpclient.HttpClient httpClient) {
+	ApacheHttpClient(final org.apache.http.client.HttpClient httpClient) {
 		this.httpClient = httpClient;
 	}
 
@@ -43,54 +48,75 @@ public class ApacheHttpClient implements HttpClient {
 	 * (non-Javadoc)
 	 * @see simplespider.simplespider_core.http.HttpClient#createConnection(java.lang.String)
 	 */
-	public void createConnection(final String url) throws HttpException, IOException {
+	/* (non-Javadoc)
+	 * @see simplespider.simplespider.bot.http.apache.T#createConnection(java.lang.String)
+	 */
+	public void createConnection(final String url) throws ClientProtocolException, IOException {
 		ValidityHelper.checkNotEmpty("url", url);
 
-		if (this.method != null) {
+		if (this.httpGet != null) {
 			throw new IllegalStateException("There is an already open connection");
 		}
 
-		this.method = new GetMethod(url);
-		this.method.setFollowRedirects(true);
+		this.httpGet = new HttpGet(url);
 
-		this.httpClient.executeMethod(this.method);
+		// Create a local instance of cookie store
+		final CookieStore cookieStore = new BasicCookieStore();
+		// Create local HTTP context
+		final HttpContext localContext = new BasicHttpContext();
+		// Bind custom cookie store to the local context
+		localContext.setAttribute(ClientContext.COOKIE_STORE, cookieStore);
+
+		//		this.httpGet.setFollowRedirects(true);
+
+		this.httpResponse = this.httpClient.execute(this.httpGet, localContext);
 	}
 
 	/*
 	 * (non-Javadoc)
 	 * @see simplespider.simplespider_core.http.HttpClient#getStatusCode()
 	 */
+	/* (non-Javadoc)
+	 * @see simplespider.simplespider.bot.http.apache.T#getStatusCode()
+	 */
 	public int getStatusCode() {
-		checkForOpenConnection();
-		return this.method.getStatusCode();
+		return getStatusLine().getStatusCode();
 	}
 
 	/*
 	 * (non-Javadoc)
 	 * @see simplespider.simplespider_core.http.HttpClient#getStatusLine()
 	 */
+	/* (non-Javadoc)
+	 * @see simplespider.simplespider.bot.http.apache.T#getStatusLine()
+	 */
 	public StatusLine getStatusLine() {
 		checkForOpenConnection();
-		return this.method.getStatusLine();
+		return this.httpResponse.getStatusLine();
 	}
 
 	/*
 	 * (non-Javadoc)
 	 * @see simplespider.simplespider_core.http.HttpClient#getStatusText()
 	 */
+	/* (non-Javadoc)
+	 * @see simplespider.simplespider.bot.http.apache.T#getStatusText()
+	 */
 	public String getStatusText() {
-		checkForOpenConnection();
-		return this.method.getStatusText();
+		return getStatusLine().getReasonPhrase();
 	}
 
 	/*
 	 * (non-Javadoc)
 	 * @see simplespider.simplespider_core.http.HttpClient#getRedirectedUrl()
 	 */
-	public String getRedirectedUrl() throws URIException {
+	/* (non-Javadoc)
+	 * @see simplespider.simplespider.bot.http.apache.T#getRedirectedUrl()
+	 */
+	public String getRedirectedUrl() {
 		checkForOpenConnection();
-		final URI uri = this.method.getURI();
-		final String realBaseUrl = uri.getURI();
+		final URI uri = this.httpGet.getURI();
+		final String realBaseUrl = uri.toString();
 		return realBaseUrl;
 	}
 
@@ -98,35 +124,46 @@ public class ApacheHttpClient implements HttpClient {
 	 * (non-Javadoc)
 	 * @see simplespider.simplespider_core.http.HttpClient#getResponseBodyAsStream()
 	 */
+	/* (non-Javadoc)
+	 * @see simplespider.simplespider.bot.http.apache.T#getResponseBodyAsStream()
+	 */
 	public InputStream getResponseBodyAsStream() throws IOException {
 		checkForOpenConnection();
-		return this.method.getResponseBodyAsStream();
+		return this.httpResponse.getEntity().getContent();
 	}
 
 	/*
 	 * (non-Javadoc)
 	 * @see simplespider.simplespider_core.http.HttpClient#releaseConnection()
 	 */
+	/* (non-Javadoc)
+	 * @see simplespider.simplespider.bot.http.apache.T#releaseConnection()
+	 */
 	public void releaseConnection() {
 		checkForOpenConnection();
-		this.method.releaseConnection();
-		this.method = null;
+		this.httpGet.abort();
+		this.httpGet = null;
+		this.httpResponse = null;
 	}
 
 	private void checkForOpenConnection() {
-		if (this.method == null) {
+		if (this.httpResponse == null || this.httpGet == null) {
 			throw new IllegalStateException("There is no open connection");
 		}
 	}
 
+	/* (non-Javadoc)
+	 * @see simplespider.simplespider.bot.http.apache.T#getMimeType()
+	 */
 	public String getMimeType() {
 		checkForOpenConnection();
-		final Header contentTypeHeader = this.method.getResponseHeader("Content-Type");
-		if (contentTypeHeader == null) {
+
+		final Header contentType = this.httpResponse.getEntity().getContentType();
+		if (contentType == null) {
 			return null;
 		}
 
-		final String contentTypeValue = contentTypeHeader.getValue();
+		final String contentTypeValue = contentType.getValue();
 		if (contentTypeValue == null) {
 			return null;
 		}
--- src/main/java/simplespider/simplespider/bot/http/apache/ApacheHttpClientFactory.java
+++ src/main/java/simplespider/simplespider/bot/http/apache/ApacheHttpClientFactory.java
@@ -17,56 +17,89 @@
  */
 package simplespider.simplespider.bot.http.apache;
 
+import java.io.IOException;
+
 import org.apache.commons.configuration.Configuration;
-import org.apache.commons.httpclient.HostConfiguration;
-import org.apache.commons.httpclient.HttpConnectionManager;
-import org.apache.commons.httpclient.HttpState;
-import org.apache.commons.httpclient.ProxyHost;
-import org.apache.commons.httpclient.cookie.CookiePolicy;
-import org.apache.commons.httpclient.params.HttpClientParams;
-import org.apache.commons.httpclient.params.HttpConnectionManagerParams;
-import org.apache.commons.httpclient.protocol.Protocol;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.http.Header;
+import org.apache.http.HeaderElement;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpException;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpRequestInterceptor;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpResponseInterceptor;
+import org.apache.http.HttpVersion;
+import org.apache.http.client.params.ClientPNames;
+import org.apache.http.client.params.CookiePolicy;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.params.ConnManagerParams;
+import org.apache.http.conn.params.ConnRoutePNames;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.CoreConnectionPNames;
+import org.apache.http.params.CoreProtocolPNames;
+import org.apache.http.params.HttpParams;
+import org.apache.http.params.HttpProtocolParams;
+import org.apache.http.protocol.HttpContext;
 
 import simplespider.simplespider.bot.http.HttpClient;
 import simplespider.simplespider.bot.http.HttpClientFactory;
-import simplespider.simplespider.bot.http.apache.ssl.TrustAllSSLProtocolSocketFactory;
 import simplespider.simplespider.util.ValidityHelper;
 
 public class ApacheHttpClientFactory implements HttpClientFactory {
 
-	private static final Log	LOG												= LogFactory.getLog(ApacheHttpClientFactory.class);
+	private static final Log		LOG												= LogFactory.getLog(ApacheHttpClientFactory.class);
 
-	private static final String	HTTP_CLIENT_USER_AGENT_DEFAULT					= "Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 6.0)";
-	private static final String	HTTP_CLIENT_USER_AGENT							= "http.client.user-agent";
+	private static final String		HTTP_CLIENT_USER_AGENT_DEFAULT					= "Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 6.0)";
+	private static final String		HTTP_CLIENT_USER_AGENT							= "http.client.user-agent";
 
-	private static final String	HTTP_CLIENT_SOCKET_TIMEOUT_SECONDS				= "http.client.socket.timeout-seconds";
-	private static final int	HTTP_CLIENT_SOCKET_TIMEOUT_SECONDS_DEFAULT		= 30;
+	private static final String		HTTP_CLIENT_SOCKET_TIMEOUT_SECONDS				= "http.client.socket.timeout-seconds";
+	private static final int		HTTP_CLIENT_SOCKET_TIMEOUT_SECONDS_DEFAULT		= 30;
 
-	private static final String	HTTP_CLIENT_CONNECTION_TIMEOUT_SECONDS			= "http.client.connection.timeout-seconds";
-	private static final int	HTTP_CLIENT_CONNECTION_TIMEOUT_SECONDS_DEFAULT	= 30;
+	private static final String		HTTP_CLIENT_CONNECTION_TIMEOUT_SECONDS			= "http.client.connection.timeout-seconds";
+	private static final int		HTTP_CLIENT_CONNECTION_TIMEOUT_SECONDS_DEFAULT	= 30;
 
-	private final ProxyHost		proxyHost;
-	private final Configuration	configuration;
+	private static final String		HTTP_CLIENT_MAX_TOTAL_CONNECTIONS				= "http.client.connection.max-total";
+	private static final int		HTTP_CLIENT_MAX_TOTAL_CONNECTIONS_DEFAULT		= 4;
+
+	private final HttpHost			proxyHost;
+	private final Configuration		configuration;
+
+	private ClientConnectionManager	connectionManager;
 
 	public ApacheHttpClientFactory(final Configuration configuration) {
 		this.configuration = configuration;
 		this.proxyHost = null;
-		setupSsl();
+		initConnectionManager();
 	}
 
 	public ApacheHttpClientFactory(final Configuration configuration, final String proxyServer, final int proxyPort) {
 		ValidityHelper.checkNotEmpty("proxyServer", proxyServer);
-
 		this.configuration = configuration;
-
-		this.proxyHost = new ProxyHost(proxyServer, proxyPort);
-		setupSsl();
+		this.proxyHost = new HttpHost(proxyServer, proxyPort, "http");
+		initConnectionManager();
 	}
 
-	private void setupSsl() {
-		Protocol.registerProtocol("https", new Protocol("https", new TrustAllSSLProtocolSocketFactory(), 443));
+	private void initConnectionManager() {
+		// general setup
+		final SchemeRegistry supportedSchemes = new SchemeRegistry();
+
+		// Register the "http" and "https" protocol schemes, they are
+		// required by the default operator to look up socket factories.
+		supportedSchemes.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
+		supportedSchemes.register(new Scheme("https", SSLSocketFactory.getSocketFactory(), 443));
+
+		final HttpParams params = buildParameters();
+
+		this.connectionManager = new ThreadSafeClientConnManager(params, supportedSchemes);
 	}
 
 	/*
@@ -74,6 +107,54 @@ public class ApacheHttpClientFactory implements HttpClientFactory {
 	 * @see simplespider.simplespider_core.http.HttpClientFactory#buildHttpClient()
 	 */
 	public HttpClient buildHttpClient() {
+
+		final HttpParams params = buildParameters();
+
+		final DefaultHttpClient httpClient = new DefaultHttpClient(this.connectionManager, params);
+
+		if (this.proxyHost != null) {
+			httpClient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, this.proxyHost);
+		}
+
+		addGzipSupport(httpClient);
+
+		return new ApacheHttpClient(httpClient);
+	}
+
+	private void addGzipSupport(final DefaultHttpClient httpClient) {
+		httpClient.addRequestInterceptor(new HttpRequestInterceptor() {
+
+			@Override
+			public void process(final HttpRequest request, final HttpContext context) throws HttpException, IOException {
+				if (!request.containsHeader("Accept-Encoding")) {
+					request.addHeader("Accept-Encoding", "gzip");
+				}
+			}
+
+		});
+
+		httpClient.addResponseInterceptor(new HttpResponseInterceptor() {
+
+			public void process(final HttpResponse response, final HttpContext context) throws HttpException, IOException {
+				final HttpEntity entity = response.getEntity();
+				final Header ceheader = entity.getContentEncoding();
+				if (ceheader != null) {
+					final HeaderElement[] codecs = ceheader.getElements();
+					for (int i = 0; i < codecs.length; i++) {
+						if (codecs[i].getName().equalsIgnoreCase("gzip")) {
+							response.setEntity(new GzipDecompressingEntity(response.getEntity()));
+							return;
+						}
+					}
+				}
+			}
+
+		});
+	}
+
+	private HttpParams buildParameters() {
+		final String userAgent = this.configuration.getString(HTTP_CLIENT_USER_AGENT, HTTP_CLIENT_USER_AGENT_DEFAULT);
+
 		int connectionTimeoutSeconds = this.configuration.getInt(HTTP_CLIENT_CONNECTION_TIMEOUT_SECONDS,
 				HTTP_CLIENT_CONNECTION_TIMEOUT_SECONDS_DEFAULT);
 		if (connectionTimeoutSeconds <= 0) {
@@ -81,39 +162,39 @@ public class ApacheHttpClientFactory implements HttpClientFactory {
 					+ HTTP_CLIENT_CONNECTION_TIMEOUT_SECONDS_DEFAULT);
 			connectionTimeoutSeconds = HTTP_CLIENT_CONNECTION_TIMEOUT_SECONDS_DEFAULT;
 		}
-		final int socketTimeoutSeconds = this.configuration.getInt(HTTP_CLIENT_SOCKET_TIMEOUT_SECONDS, HTTP_CLIENT_SOCKET_TIMEOUT_SECONDS_DEFAULT);
+
+		int socketTimeoutSeconds = this.configuration.getInt(HTTP_CLIENT_SOCKET_TIMEOUT_SECONDS, HTTP_CLIENT_SOCKET_TIMEOUT_SECONDS_DEFAULT);
 		if (socketTimeoutSeconds <= 0) {
 			LOG.warn("Configuration " + HTTP_CLIENT_SOCKET_TIMEOUT_SECONDS + " is invalid. Using default value: "
 					+ HTTP_CLIENT_SOCKET_TIMEOUT_SECONDS_DEFAULT);
-			connectionTimeoutSeconds = HTTP_CLIENT_SOCKET_TIMEOUT_SECONDS_DEFAULT;
-		}
-		final String userAgent = this.configuration.getString(HTTP_CLIENT_USER_AGENT, HTTP_CLIENT_USER_AGENT_DEFAULT);
-
-		final org.apache.commons.httpclient.HttpClient httpClient = new org.apache.commons.httpclient.HttpClient();
-
-		if (this.proxyHost != null) {
-			final HostConfiguration hostConfiguration = httpClient.getHostConfiguration();
-			hostConfiguration.setProxyHost(this.proxyHost);
+			socketTimeoutSeconds = HTTP_CLIENT_SOCKET_TIMEOUT_SECONDS_DEFAULT;
 		}
 
-		final HttpConnectionManager httpConnectionManager = httpClient.getHttpConnectionManager();
-		final HttpConnectionManagerParams httpConnectionManagerParams = httpConnectionManager.getParams();
-		httpConnectionManagerParams.setConnectionTimeout(connectionTimeoutSeconds * 1000);
+		int maxTotalConnections = this.configuration.getInt(HTTP_CLIENT_MAX_TOTAL_CONNECTIONS, HTTP_CLIENT_MAX_TOTAL_CONNECTIONS_DEFAULT);
+		if (maxTotalConnections <= 0) {
+			LOG.warn("Configuration " + HTTP_CLIENT_MAX_TOTAL_CONNECTIONS + " is invalid. Using default value: "
+					+ HTTP_CLIENT_MAX_TOTAL_CONNECTIONS_DEFAULT);
+			maxTotalConnections = HTTP_CLIENT_MAX_TOTAL_CONNECTIONS_DEFAULT;
+		}
 
-		// Get initial state object
-		final HttpState initialState = new HttpState();
-		httpClient.setState(initialState);
+		// prepare parameters
+		final HttpParams params = new BasicHttpParams();
+		HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
+		HttpProtocolParams.setContentCharset(params, "UTF-8");
+		HttpProtocolParams.setUseExpectContinue(params, true);
 
-		final HttpClientParams clientParams = httpClient.getParams();
-		// More browser like behavior
-		clientParams.setCookiePolicy(CookiePolicy.BROWSER_COMPATIBILITY);
-		// More browser like behavior
-		clientParams.makeLenient();
-		// Setting client global socket timeout
-		clientParams.setSoTimeout(socketTimeoutSeconds * 1000);
-		// Setting user agent
-		clientParams.setParameter("http.useragent", userAgent);
+		ConnManagerParams.setMaxTotalConnections(params, maxTotalConnections);
 
-		return new ApacheHttpClient(httpClient);
+		params.setParameter(CoreProtocolPNames.WAIT_FOR_CONTINUE, Integer.valueOf(connectionTimeoutSeconds));
+		params.setParameter(CoreProtocolPNames.USER_AGENT, userAgent);
+		params.setParameter(CoreConnectionPNames.SO_TIMEOUT, Integer.valueOf(socketTimeoutSeconds * 1000));
+		params.setParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, Integer.valueOf(connectionTimeoutSeconds * 1000));
+
+		params.setParameter(ClientPNames.ALLOW_CIRCULAR_REDIRECTS, Boolean.TRUE);
+		params.setParameter(ClientPNames.COOKIE_POLICY, CookiePolicy.BROWSER_COMPATIBILITY);
+		params.setParameter(ClientPNames.HANDLE_REDIRECTS, Boolean.TRUE);
+		params.setParameter(ClientPNames.REJECT_RELATIVE_REDIRECT, Boolean.FALSE);
+		return params;
 	}
+
 }
